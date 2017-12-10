7ae2371c36860d0cab595faf89bf70502686ef4d
Ognjen Gatalo <ognjeng95@gmail.com>
2017-12-10 22:58:11 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,10 @@
+const config = require('./config');
+var Twit = require('twit');
+
+var twitClient = new Twit({
+  consumer_key:         config.consumer_key,
+  consumer_secret:      config.consumer_secret,
+  access_token:         config.access_token,
+  access_token_secret:  config.access_token_secret,
+  timeout_ms:           60*1000,
+})
