4cd7ae5fb9d2a75fffc95401657e4e1dcb084bd8
Ognjen Gatalo <ognjeng95@gmail.com>
2017-12-10 23:18:45 +0100

    Added core functions

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Twitter bot that tweets future bitcoin value predictions.
 
 Visit https://twitter.com/coin_instinct
+Bitcoin value API powered by Blockchain.info https://blockchain.info/
+Bitcoin historical API powered by CoinDesk https://www.coindesk.com
--- bot.js
+++ bot.js
@@ -1,5 +1,13 @@
 const config = require('./config');
 var Twit = require('twit');
+var fetch = require('node-fetch');
+
+var bitcoinData = {
+  results: '',
+  history: ''
+};
+var tickerApiUrl = "https://blockchain.info/ticker";
+var chartsApiUrl = "https://api.coindesk.com/v1/bpi/historical/close.json";
 
 var twitClient = new Twit({
   consumer_key:         config.consumer_key,
@@ -7,4 +15,51 @@ var twitClient = new Twit({
   access_token:         config.access_token,
   access_token_secret:  config.access_token_secret,
   timeout_ms:           60*1000,
-})
+});
+
+refresh(tickerApiUrl);
+queryChartHistory(chartsApiUrl,'');
+
+
+
+
+/**
+ * Refreshes bitcoin prices
+ * @param {*String} tickerApiUrl API endpoint for getting the latest bitcoin prices 
+ */
+function refresh(tickerApiUrl) {
+  const request = async (tickerApiUrl) => {
+    var results = await fetch(tickerApiUrl);
+    results = await results.json();
+
+    bitcoinData.results = results;
+    if(bitcoinData.results) {console.log('Blockchain API works!'); console.log(bitcoinData)}
+    else console.log('Blockchain API is down at the moment.');
+  }
+
+    request(tickerApiUrl);
+
+    // Enable in prod
+    // setInterval(() => {
+    //   refresh(tickerApiUrl,chartsApiUrl);
+    //   console.log(bitcoinData.results);
+    // },60000);
+}
+
+/**
+ * Queries the CoinDesk api for specific date span
+ * @param {*String} chartsApiUrl API endpoint for getting bitcoin history values
+ * @param {*String} parameters query parameters
+ */
+function queryChartHistory(chartsApiUrl, parameters) {
+  const request = async (chartsApiUrl,parameters) => {
+    var history = await fetch(chartsApiUrl+'?'+parameters);
+    history = await history.json();
+    
+    bitcoinData.history = history;
+    if(bitcoinData.history) {console.log('History charts API works'); console.log(bitcoinData)}
+    else console.log('History charts API is down at the moment.');
+  }
+
+  request(chartsApiUrl,parameters);
+}
