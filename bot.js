c78baf6e7fcb79f2dfc907a968e2bfcaed2d8b24
Ognjen Gatalo <ognjeng95@gmail.com>
2017-12-11 02:04:56 +0100

    Continue tomorrow

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ var Twit = require('twit');
 var fetch = require('node-fetch');
 
 var bitcoinData = {
-  results: '',
-  history: ''
+  results: ''
 };
 var tickerApiUrl = "https://blockchain.info/ticker";
 var chartsApiUrl = "https://api.coindesk.com/v1/bpi/historical/close.json";
@@ -17,17 +16,52 @@ var twitClient = new Twit({
   timeout_ms:           60*1000,
 });
 
-refresh(tickerApiUrl);
-queryChartHistory(chartsApiUrl,'');
+//refreshBitcoinPrices(tickerApiUrl);
+
+work();
 
 
+// async function fetchBtc(tickerApiUrl) {
+//   const response = await fetch(tickerApiUrl);
+//   const btc = await response.json();
+//   return btc;
+// }
 
+// async function fetch2(tickerApiUrl) {
+//   const response = await fetch(tickerApiUrl);
+//   const btc = await response.json();
+//   return btc;
+// }
+
+function work() {
+  setInterval(function(){
+    
+    // fetchBtc(tickerApiUrl).then((data) => {
+    //   console.log('Izvrsena prva');
+    //   fetch2(tickerApiUrl).then( (data) => {
+    //     console.log('Izvrsena druga')
+    //   } )
+    // })
+
+    collectTweetDemands().then(function(response) {
+      console.log(response.data.statuses.map(status => status.text));
+    })
+
+  },3000);
+}
 
 /**
- * Refreshes bitcoin prices
+ * Collects users demands, how much days in the future to predict the value
+ */
+async function collectTweetDemands() {
+  return await twitClient.get('search/tweets', {q: 'bitcoin since:2017-12-11T02:00', count: 2});
+}
+
+/**
+ * Refreshes bitcoin prices minute before the main thread starts the work
  * @param {*String} tickerApiUrl API endpoint for getting the latest bitcoin prices 
  */
-function refresh(tickerApiUrl) {
+function refreshBitcoinPrices(tickerApiUrl) {
   const request = async (tickerApiUrl) => {
     var results = await fetch(tickerApiUrl);
     results = await results.json();
@@ -41,7 +75,7 @@ function refresh(tickerApiUrl) {
 
     // Enable in prod
     // setInterval(() => {
-    //   refresh(tickerApiUrl,chartsApiUrl);
+    //   refreshBitcoinPrices(tickerApiUrl,chartsApiUrl);
     //   console.log(bitcoinData.results);
     // },60000);
 }
@@ -52,14 +86,5 @@ function refresh(tickerApiUrl) {
  * @param {*String} parameters query parameters
  */
 function queryChartHistory(chartsApiUrl, parameters) {
-  const request = async (chartsApiUrl,parameters) => {
-    var history = await fetch(chartsApiUrl+'?'+parameters);
-    history = await history.json();
-    
-    bitcoinData.history = history;
-    if(bitcoinData.history) {console.log('History charts API works'); console.log(bitcoinData)}
-    else console.log('History charts API is down at the moment.');
-  }
-
-  request(chartsApiUrl,parameters);
+  
 }
